ee9346626b5fb4b2a5dbadddbb821132183bfe4c
Elaheh Aghabalazadeh <eaghabalazade1376@gmail.com>
2024-06-19 00:16:06 +0330

    some change

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+            FormsModule,
+            MatFormFieldModule,
+            MatInputModule,
+            MatIconModule,
+            MatGridListModule,
+            MatButtonModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
