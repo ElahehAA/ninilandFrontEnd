9aafee10a7bce4be0756e41821b3b43275375b90
Elaheh Aghabalazadeh <eaghabalazade1376@gmail.com>
2024-07-05 01:58:40 +0330

    Add fontAwesome

--- src/app/main-page/nav-bar/nav-bar.component.ts
+++ src/app/main-page/nav-bar/nav-bar.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faTruck } from '@fortawesome/free-solid-svg-icons';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule,FontAwesomeModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
-
+  icon=faTruck;
 }
