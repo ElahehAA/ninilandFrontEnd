6823b48d930adc44a0827d7f3f61c432088f4879
Elaheh Aghabalazadeh <eaghabalazade1376@gmail.com>
2024-08-28 23:43:21 +0330

    Edit navbar

--- src/app/main-page/nav-bar/nav-bar.component.ts
+++ src/app/main-page/nav-bar/nav-bar.component.ts
@@ -3,14 +3,19 @@ import { MatIconModule } from '@angular/material/icon';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faTruck } from '@fortawesome/free-solid-svg-icons';
 import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [MatIconModule,FontAwesomeModule],
+  imports: [MatIconModule,FontAwesomeModule,],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
   icon=faTruck;
+  icon2=faChevronLeft;
+
+
+
 }
